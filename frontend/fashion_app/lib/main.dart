384448b52da795b6a6a64a3e949881c3e4a5c589
ChrisShobe <chrisshobe2000@gmail.com>
2023-12-23 14:05:05 -0800

    second page demo

--- frontend/fashion_app/lib/main.dart
+++ frontend/fashion_app/lib/main.dart
@@ -4,22 +4,6 @@ import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
-//void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  //final settingsController = SettingsController(SettingsService());
-
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  //await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
- // runApp(MyApp());
-  //(settingsController: settingsController));
-//}
-
 void main() {
   runApp(MyApp());
 }
@@ -73,12 +57,30 @@ class MyHomePage extends StatelessWidget {
             SizedBox(height: 20.0),
             ElevatedButton(
               onPressed: () {
-                // Add your action here
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
               },
-              child: Text('Press me'),
+              child: Text('Continue'),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+// second page
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: Text('This is the second page!'),
       ),
     );
   }
